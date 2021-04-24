b69831365cbab6f7d36e99995f321365f93d1695
Rodrigo Jorge <rodrigodoj@gmail.com>
2021-04-24 12:29:48 -0300

    melhorando o codigo

--- app/src/main/java/com/dojo/nowsportsreviewapp/MainActivity.java
+++ app/src/main/java/com/dojo/nowsportsreviewapp/MainActivity.java
@@ -2,7 +2,6 @@ package com.dojo.nowsportsreviewapp;
 
 import android.content.Intent;
 import android.net.Uri;
-import android.os.AsyncTask;
 import android.os.Bundle;
 import com.google.android.material.navigation.NavigationView;
 
@@ -14,25 +13,10 @@ import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
-import org.json.JSONArray;
-import org.json.JSONException;
-import org.json.JSONObject;
-
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.net.HttpURLConnection;
-import java.net.MalformedURLException;
-import java.net.URL;
-import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
     private AppBarConfiguration mAppBarConfiguration;
-    public ArrayList<Noticia> listaNoticia;
-    public JsonTask meuJson;
-    private static String minhaUrlTeste = "https://www.json-generator.com/api/json/get/cvlKbvCoaG?indent=2";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -49,13 +33,6 @@ public class MainActivity extends AppCompatActivity {
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
         NavigationUI.setupActionBarWithNavController(this, navController, mAppBarConfiguration);
         NavigationUI.setupWithNavController(navigationView, navController);
-        listaNoticia = new ArrayList<>();
-        meuJson = new JsonTask();
-        meuJson.execute(minhaUrlTeste);
-
-
-
-
     }
 
     public void enviarEmail(){
@@ -78,78 +55,4 @@ public class MainActivity extends AppCompatActivity {
                 || super.onSupportNavigateUp();
     }
 
-    public class JsonTask extends AsyncTask<String, String, String> {
-
-        protected void onPreExecute() {
-            super.onPreExecute();
-        }
-
-        protected String doInBackground(String... params) {
-
-            HttpURLConnection connection = null;
-            BufferedReader reader = null;
-
-            try {
-                URL url = new URL(params[0]);
-                connection = (HttpURLConnection) url.openConnection();
-                connection.connect();
-
-                InputStream stream = connection.getInputStream();
-                reader = new BufferedReader(new InputStreamReader(stream));
-                StringBuffer buffer = new StringBuffer();
-
-                String line = "";
-
-                while ((line = reader.readLine()) != null) {
-                    buffer.append(line + "\n");
-                }
-                return buffer.toString();
-
-            } catch (MalformedURLException e) {
-                e.printStackTrace();
-            } catch (IOException e) {
-                e.printStackTrace();
-            } finally {
-                if (connection != null) {
-                    connection.disconnect();
-                }
-                try {
-                    if (reader != null) {
-                        reader.close();
-                    }
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
-            }
-            return null;
-        }
-
-        @Override
-        protected void onPostExecute(String result) {
-            super.onPostExecute(result);
-
-            try {
-                JSONObject listaJson = new JSONObject(result);
-                JSONArray rodada = listaJson.getJSONArray("questionario");
-                String tituloCampeonato = listaJson.getString("titulo");
-
-                for (int i = 0; i < rodada.length(); i++) {
-                    JSONObject noticia = rodada.getJSONObject(i);;
-
-                    String timeCasa = noticia.getString("timeCasa");
-                    String timeVisitante = noticia.getString("timeVisitante");
-                    String siglaCasa = noticia.getString("siglaCasa");
-                    String siglaVisitante = noticia.getString("siglaVisitante");
-                    String placar = noticia.getString("placar");
-
-
-                    Noticia minhaNoticia = new Noticia(timeCasa, timeVisitante, siglaCasa, siglaVisitante, placar, tituloCampeonato);
-                    listaNoticia.add(minhaNoticia);
-                }
-
-            } catch (JSONException e) {
-                e.printStackTrace();
-            }
-        }
-    }
 }
